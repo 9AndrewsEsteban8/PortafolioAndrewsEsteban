e83aaa0abfeb002b3eceb67cc4d0a4811d63247f
9AndrewsEsteban8 <andrewsesteban7@gmail.com>
2024-02-05 19:31:49 -0500

    modification nav

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,28 +1,24 @@
-import React from 'react'
-import {faFacebook,faLinkedin,faInstagram} from '@fortawesome/free-brands-svg-icons'
-import SocialIcon from './SocialIcon';
+import React, { useState } from "react";
 import iconPage from '../images/Logo.png';
 
 
-
-const socialLinks = [
-    { href: 'https://www.facebook.com/Esteban.Andrews', icon: faFacebook },
-    { href: 'http://www.linkedin.com/in/andrewsesteban', icon: faLinkedin },
-    { href: 'https://www.instagram.com/andrews_esteban/', icon: faInstagram },
-];
-
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false)
     return (
-    <div>
-        <header>
-        <img src={iconPage} alt="Icono de página"/>
-        <nav className='social_networks'>
-        {socialLinks.map((link, index) => (
-            <SocialIcon key={index} href={link.href} icon={link.icon} />
-        ))}
-        </nav>
-        </header>
-    </div>
+        <div className='navbar'>
+            <img className="nav_logo" src={iconPage} alt="Icono de página" />
+            <nav className={`nav_items ${isOpen && "open"}`}>
+                <a href="#">Perfil</a>
+                <a href="#">Educacio</a>
+                <a href="#">Habilidades</a>
+                <a href="#">Contacto</a>
+            </nav>
+            <div className={`nav_toggle ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)} >
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
     );
 };
 
