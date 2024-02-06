45ebb34153c0bb5261d5b2f66cd45b8b00ab5a92
9AndrewsEsteban8 <andrewsesteban7@gmail.com>
2024-02-06 08:55:40 -0500

    SocialNetworks

--- src/App.js
+++ src/App.js
@@ -10,27 +10,43 @@ import Projects from './Components/Projects';
 import firstproject from '../src/images/Project_Sabuja.png'
 import secondproject from '../src/images/Project_Pookemos.png'
 import thirdproject from '../src/images/Project_Lading.png'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faCloudDownload } from '@fortawesome/free-solid-svg-icons';
+import SocialIcon from '../src/Components/SocialIcon';
+import Curriculum from '../src/Documentes/CV Andresw Esteban.pdf'
 
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
+      <Header />
+      <Home />
       <section className='Container_cards'>
         <div className='containers_cardsred'>
           <div className='aboutme'>
             <h2>Sobre mi</h2>
-            <Cards>         
-              <p>Soy un apasionado desarrollador web Frontend con experiencia en la creación de interfaces atractivas con responsivas.<br/> 
-              He trabajado con tecnologías como React,Git y Github; centrándome en el desarrollo web. <br/> 
-              Mi principal objetivo es convertir diseños creativos en código funcional. <br/> 
-              Mi experiencia como Guardián me ha generado una experiencia de 4 años en habilidades como el compromiso y el trabajo en equipo.<br/> 
-              De esta manera, logro proporcionar soluciones digitales de alta calidad en conjunto con colegas de diferentes áreas de especialización.</p>
+            <Cards>
+              <p>Soy un apasionado desarrollador web Frontend con experiencia en la creación de interfaces atractivas con responsivas.<br />
+                He trabajado con tecnologías como React,Git y Github; centrándome en el desarrollo web. <br />
+                Mi principal objetivo es convertir diseños creativos en código funcional. <br />
+                Mi experiencia como Guardián me ha generado una experiencia de 4 años en habilidades como el compromiso y el trabajo en equipo.<br />
+                De esta manera, logro proporcionar soluciones digitales de alta calidad en conjunto con colegas de diferentes áreas de especialización.</p>
             </Cards>
           </div>
-        <Tecnology/>
+          <h2>Redes y Curriculum</h2>
+          <div className='container-socialmedia'>
+            <SocialIcon
+              href={'https://www.linkedin.com/in/andrewsesteban/'}
+              icon={faLinkedin}
+              text={'Linkedin'} />
+
+            <SocialIcon
+              href={Curriculum}
+              icon={faCloudDownload}
+              text={'Descargar CV'} />
+          </div>
+          <Tecnology />
         </div>
         <hr className="line" />
         <div className='text_projects'>
@@ -39,33 +55,33 @@ function App() {
         </div>
         <div className='repo'>
           <Projects
-          link='https://sabujcha-web.vercel.app/' 
-          imgSrc={firstproject}   
-          imgAlt='foto_primerproyecto'  
-          text= 'Tienda online'
-          linkrepo='https://github.com/JorgeT0rr3s/Sabujcha-web.git'
+            link='https://sabujcha-web.vercel.app/'
+            imgSrc={firstproject}
+            imgAlt='foto_primerproyecto'
+            text='Tienda online'
+            linkrepo='https://github.com/JorgeT0rr3s/Sabujcha-web.git'
           />
           <Projects
-          link='https://pokemon-api-red.vercel.app/' 
-          imgSrc={secondproject}   
-          imgAlt='foto_segundoproyecto'
-          text= 'Pokedex' 
-          linkrepo='https://github.com/JorgeT0rr3s/Pokemon-api.git'   
+            link='https://pokemon-api-red.vercel.app/'
+            imgSrc={secondproject}
+            imgAlt='foto_segundoproyecto'
+            text='Pokedex'
+            linkrepo='https://github.com/JorgeT0rr3s/Pokemon-api.git'
           />
           <Projects
-          link='https://genero-front-six.vercel.app/' 
-          imgSrc={thirdproject}   
-          imgAlt='foto_tercerproyecto' 
-          text= 'Landing page'
-          linkrepo='https://github.com/programateacademy/genero-front.git' 
+            link='https://genero-front-six.vercel.app/'
+            imgSrc={thirdproject}
+            imgAlt='foto_tercerproyecto'
+            text='Landing page'
+            linkrepo='https://github.com/programateacademy/genero-front.git'
           />
         </div>
       </section>
       <div className='container_contactfrom'>
-        <Contact_me/>
-        <From/>
+        <Contact_me />
+        <From />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
